6f8475e0ac258b28a4f2fbab6e91bad867b3033e
Anand Pap <anandpap@live.com>
2023-02-26 13:59:56 +0100

    added hamburger menu and styled it

--- src/components/HomeNavBar.tsx
+++ src/components/HomeNavBar.tsx
@@ -1,55 +1,65 @@
-import { useLocation, useNavigate } from "react-router-dom";
-import { useAppDispatch, useAppSelector } from "../redux/hooks";
-import { setLoading } from "../redux/movies";
 import LottieDarkModeSwitch from "../components-reusable/LottieDarkModeSwitch";
 import tmdbIcon from "../assets/tmdbIcon.svg";
+import ChangePageButton from "./ChangePageButton";
+import { useState } from "react";
+import { useAppSelector } from "../redux/hooks";
 
 type HomeNavBarProps = {
   searchParam: string | null;
 };
 
 const HomeNavBar = ({ searchParam }: HomeNavBarProps) => {
-  const dispatch = useAppDispatch();
-  const darkMode = useAppSelector((state) => state.movies.darkMode);
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const setPage = (pageName: string) => {
-    if (pageName !== location.pathname) {
-      if (searchParam) {
-        navigate(`${pageName}?search=${searchParam}`);
-        dispatch(setLoading(true));
-      } else {
-        navigate(`${pageName}`);
-      }
-    }
-  };
+  const [displayHamburgerContent, setDisplayHamburgerContent] = useState(false);
+  const darkmode = useAppSelector((state) => state.movies.darkMode);
 
   return (
-    <div className="home-nav-bar">
-      <img className="tmdb-icon" src={tmdbIcon} alt="TMDB Icon" />
-      <div className="change-page-buttons-wrapper">
-        <button
-          className={`nav-button ${
-            location.pathname === "/movies" ? "selected-button" : null
-          } ${darkMode ? "dark" : "light"}`}
-          type="button"
-          onClick={() => setPage("/movies")}
+    <>
+      <div className="home-nav-bar">
+        <img className="tmdb-icon" src={tmdbIcon} alt="TMDB Icon" />
+        <div className="nav-buttons-wrapper">
+          <div className="nav-buttons-wrapper">
+            <ChangePageButton
+              className="nav-button"
+              page="/movies"
+              searchParam={searchParam}
+            />
+            <ChangePageButton
+              className="nav-button"
+              page="/tvshows"
+              searchParam={searchParam}
+            />
+          </div>
+          <LottieDarkModeSwitch />
+        </div>
+        <div
+          className={`hamburger-menu-wrapper ${
+            displayHamburgerContent ? "hamburger-open" : "hamburger-close"
+          }`}
         >
-          Movies
-        </button>
-        <button
-          className={`nav-button ${
-            location.pathname === "/tvshows" ? "selected-button" : null
-          } ${darkMode ? "dark" : "light"}`}
-          type="button"
-          onClick={() => setPage("/tvshows")}
-        >
-          TV Shows
-        </button>
+          <div
+            className="hamburger-menu"
+            onClick={() => setDisplayHamburgerContent((s) => !s)}
+          ></div>
+          <div
+            className={`hamburger-menu-content ${
+              displayHamburgerContent ? "hamburger-open" : "hamburger-close"
+            } ${darkmode ? "dark" : "light"}`}
+          >
+            <ChangePageButton
+              className="hamburger-nav-button"
+              page="/movies"
+              searchParam={searchParam}
+            />
+            <ChangePageButton
+              className="hamburger-nav-button"
+              page="/tvshows"
+              searchParam={searchParam}
+            />
+            <LottieDarkModeSwitch />
+          </div>
+        </div>
       </div>
-      <LottieDarkModeSwitch />
-    </div>
+    </>
   );
 };
 
