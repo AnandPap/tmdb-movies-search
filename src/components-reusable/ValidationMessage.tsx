a6e5bfc3477e3fd3bf14f3fd2c250bddf9195ad3
Anand Pap <anandpap@live.com>
2023-03-04 18:40:21 +0100

    fixed bug in dark mode look in validation message

--- src/components-reusable/ValidationMessage.tsx
+++ src/components-reusable/ValidationMessage.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useAppSelector } from "../redux/hooks";
+import { setDarkMode } from "../redux/movies";
+
 type ValidationMessageProps = {
   className: string;
   text: string;
 };
 
 const ValidationMessage = ({ className, text }: ValidationMessageProps) => {
-  let darkMode = localStorage.getItem("darkMode");
-  darkMode ? (darkMode = JSON.parse(darkMode)) : null;
+  const darkMode = useAppSelector((state) => state.movies.darkMode);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    let darkMode = localStorage.getItem("darkMode");
+    if (darkMode) dispatch(setDarkMode(JSON.parse(darkMode)));
+  }, []);
 
   return (
     <div
